e520b5a805a27220b75d4113fa95053f84b390d9
ConteMan <boxiaozhi.bolu@gmail.com>
2021-04-14 19:39:54 +0800

    :sparkles:  feat: init;

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,59 +1,33 @@
 module.exports = {
-    title: 'ConteMan World',
-    description: "ConteMan can't stop.",
-    //theme: 'reco',
-    theme: require.resolve('./theme'),
-    themeConfig: {
-        docsDir: 'docs',
-        nav: [
-            {text: 'Home', link: '/', icon: 'reco-home'},
-            {text: 'TimeLine', link: '/timeline/', icon: 'reco-date'},
-        ],
-        type: 'blog',
-        noFoundPageByTencent: false,
-        // 博客设置
-        blogConfig: {
-            category: {
-                location: 2, // 在导航栏菜单中所占的位置，默认 2
-                text: 'Category' // 默认 “分类”
-            },
-            tag: {
-                location: 3, // 在导航栏菜单中所占的位置，默认 3
-                text: 'Tag' // 默认 “标签”
-            }
+  title: 'ConteWorld',
+  description: 'Slow down, not so much to seize.',
+  themeConfig: {
+    search: true,
+    nav: [
+      { text: 'JavaScript', link: '/develop/' },
+    ],
+    sidebar: [
+      {
+        title: 'JavaScript',
+        path: '/develop/JavaScript',
+      }
+    ],
+    sidebarDepth: 2,
+    displayAllHeaders: true,
+    smoothScroll: true,
+  },
+  plugins: {
+    '@vuepress/back-to-top': true,
+    redirect: {
+      redirectors: [
+        {
+          base: '/develop/',
+          storage: true, // 保存最后一次访问的结果到 `localStorage`，供下次重定向使用
+          alternative: [
+            'JavaScript',
+          ],
         },
-        //logo: '/head.png',
-        //authorAvatar: '/head.png',
-        // 搜索设置
-        search: true,
-        searchMaxSuggestions: 10,
-        // 自动形成侧边导航
-        sidebar: 'auto',
-        sidebarDepth: 4,
-        // 最后更新时间
-        lastUpdated: 'Last Updated',
-        // 作者
-        author: 'ConteMan',
-        // 备案号
-        record: 'xxxx',
-        // 项目开始时间
-        startYear: '2020',
-        /**
-         * 密钥 (if your blog is private)
-         */
-        friendLink: [
-            {
-                title: ' 午后南杂',
-                desc: 'Enjoy when you can, and endure when you must.',
-                email: '1156743527@qq.com',
-                link: 'https://www.recoluan.com'
-            },
-            {
-                title: 'vuepress-theme-reco',
-                desc: 'A simple and beautiful vuepress Blog & Doc theme.',
-                avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-                link: 'https://vuepress-theme-reco.recoluan.com'
-            },
-        ],
+      ],
     }
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,5 @@
 ---
-
 home: true
-title: Good
-date: 2020-07-07 11:53
-
+actionText: GO
+actionLink: /develop/JavaScript
 ---
